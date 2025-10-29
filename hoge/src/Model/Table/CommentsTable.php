26112c4167f451f753ce9acab825575cab624d19
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-10-29 19:57:23 +0900

    コメント機能実装

--- hoge/src/Model/Table/CommentsTable.php
+++ hoge/src/Model/Table/CommentsTable.php
@@ -5,27 +5,19 @@ namespace App\Model\Table;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
-class PostsTable extends Table
+class CommentsTable extends Table
 {
   public function initialize(array $config): void
   {
     $this->addBehavior('Timestamp');
-    $this->hasMany('Comments');
+    $this->belongsTo('Posts');
   }
 
   public function validationDefault(Validator $validator)
   {
     $validator
-      ->notEmpty('title')
-      ->requirePresence('title')
       ->notEmpty('body')
-      ->requirePresence('body')
-      ->add('body', [
-        'length' => [
-          'rule' => ['minLength', 10],
-          'message' => 'ダメよ'
-        ]
-      ]);
+      ->requirePresence('body');
     return $validator;
   }
 }
--- hoge/src/Template/Posts/view.ctp
+++ hoge/src/Template/Posts/view.ctp
@@ -7,3 +7,19 @@
   <?= h($post->title); ?>
 </h1>
 <p><?= nl2br(h($post->body)); ?></p>
+
+<h2>Comments<span>(<?= count($post->comments); ?>)</span></h2>
+<ul>
+  <?php foreach ($post->comments as $comment) : ?>
+    <li><?= h($comment->body) ?></li>
+  <?php endforeach ?>
+</ul>
+
+<h2>New Comments</h2>
+<?= $this->Form->create(null, [
+  'url' => ['controller' => 'Comments', 'action' => 'add']
+]); ?>
+<?= $this->Form->input('body'); ?>
+<?= $this->Form->hidden('post_id', ['value'=>$post->id]); ?>
+<?= $this->Form->button('Add'); ?>
+<?= $this->Form->end(); ?>
